0cf0926e6c2ee711589e4c13fd65be13d11130a8
“mjwz5294” <393578079@qq.com>
2018-03-05 21:55:46 +0800

    init

---
+++ WXLApp/WXLAppTests/WXLAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  WXLAppTests.swift
+//  WXLAppTests
+//
+//  Created by mac on 2018/3/5.
+//  Copyright © 2018年 mjwz5294. All rights reserved.
+//
+
+import XCTest
+@testable import WXLApp
+
+class WXLAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
