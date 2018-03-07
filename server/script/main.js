7b33289137df6672a97ba37906746a0c2d4232a4
“mjwz5294” <393578079@qq.com>
2018-03-07 20:59:40 +0800

    代码结构整理

--- server/script/main.js
+++ server/script/main.js
@@ -1,1 +1,22 @@
-console.log('启动服务器！！');
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// parse request body:
+app.use(bodyParser());
+
+// add controllers:
+app.use(controller('controllers'));
+
+app.listen(3000);
+console.log('app started at port 3000...');
