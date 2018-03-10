e37f595025eb0aae29f17f91a82f2d3888773e4d
“mjwz5294” <393578079@qq.com>
2018-03-10 16:39:59 +0800

    修改表结构

--- server/mysql/database.sql
+++ server/mysql/database.sql
@@ -1,6 +1,7 @@
 CREATE TABLE   IF NOT EXISTS  `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `writer` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `create_time` varchar(20) DEFAULT NULL,
   `modified_time` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
